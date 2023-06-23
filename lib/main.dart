dbbccc4ba02958624cf3cd636b3145029fa0d0c5
riazahmedmahin <c201060@ugrad.iiuc.ac.bd>
2023-06-23 22:40:59 +0600

    Mahin

--- lib/main.dart
+++ lib/main.dart
@@ -140,7 +140,65 @@ child: Icon(Icons.add_circle_outlined),
 ),
 
 
+      body: Container(child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: GridView(children: [
+          InkWell(
+            onTap: (){
 
+            },
+            child: Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.red,),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.home,size: 50,color: Colors.white,),
+                  Text("Home",style: TextStyle(color: Colors.white,fontSize: 30),)
+                ],),
+            ),
+          ),
+          InkWell(
+            onTap: (){
+
+            },
+            child: Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.yellow,),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.search,size: 50,color: Colors.white,),
+                  Text("Search",style: TextStyle(color: Colors.white,fontSize: 30),)
+                ],),
+            ),
+          ),
+          InkWell(
+            onTap: (){
+
+            },
+            child: Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.green,),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.settings,size: 50,color: Colors.white,),
+                  Text("Settings",style: TextStyle(color: Colors.white,fontSize: 30),)
+                ],),
+            ),
+          ),
+          InkWell(
+            onTap: (){
+
+            },
+            child: Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.grey,),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.book,size: 50,color: Colors.white,),
+                  Text("Books",style: TextStyle(color: Colors.white,fontSize: 30),)
+                ],),
+            ),
+          ),
+        ],
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,mainAxisSpacing: 10,crossAxisSpacing: 10),
+        ),
+      ),),
 
 
     );
